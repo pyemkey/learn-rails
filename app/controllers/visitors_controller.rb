189671a07623c2a96ee14438b9eabd88929364e0
Michal Kloda <py.emkey@gmail.com>
2013-12-19 20:00:50 +0100

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,5 @@
 class VisitorsController < ApplicationController 
 	def new
 		@owner = Owner.new
-		flash.now[:notice] = "I will be one of the NETGURU teammate!"
-		flash.now[:alert] = "First what I have to do is call there"
 	end
 end
