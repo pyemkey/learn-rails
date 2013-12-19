ad48933dad0def9f0a1db17d78dd3f50fe1dee21
Michal Kloda <py.emkey@gmail.com>
2013-12-19 11:00:47 +0100

    Update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,7 @@
 class VisitorsController < ApplicationController 
 	def new
 		@owner = Owner.new
-	#	render 'owners/new'
+		flash.now[:notice] = "I will be one of the NETGURU teammate!"
+		flash.now[:alert] = "First what I have to do is call there"
 	end
 end
