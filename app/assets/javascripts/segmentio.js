c3e0e16ab090b30d1003101312a2cfd065584781
Michal Kloda <py.emkey@gmail.com>
2013-12-21 20:39:01 +0100

    analytics

--- app/assets/javascripts/segmentio.js
+++ app/assets/javascripts/segmentio.js
@@ -1,9 +1,11 @@
-var analytics=analytics||(window.analytics=[]),window.analytics.methods=["identify","track","trackLink","trackForm","trackClick","trackSubmit","page","pageview","ab","alias","ready","group","on","once","off"],window.analytics.factory=function(t){return function(){var a=Array.prototype.slice.call(arguments);return a.unshift(t),window.analytics.push(a),window.analytics}};for(var i=0;i<window.analytics.methods.length;i++){var method=window.analytics.methods[i];window.analytics[method]=window.analytics.factory(method)}window.analytics.load=function(t){var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n)},window.analytics.SNIPPET_VERSION="2.0.8",
-analytics.load("hdrwb3gk2g");
-analytics.page();
-$(document).on('page:load', function() {
-console.log('page loaded');
-analytics.pageview();
-analytics.trackForm($('#new_visitor'), 'Signed Up');
-analytics.trackForm($('#new_contact'), 'Contact Request');
+window.analytics||(window.analytics=[]),window.analytics.methods=["identify","track","trackLink","trackForm","trackClick","trackSubmit","page","pageview","ab","alias","ready","group","on","once","off"],window.analytics.factory=function(t){return function(){var a=Array.prototype.slice.call(arguments);return a.unshift(t),window.analytics.push(a),window.analytics}};for(var i=0;i<window.analytics.methods.length;i++){var method=window.analytics.methods[i];window.analytics[method]=window.analytics.factory(method)}window.analytics.load=function(t){var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n)},window.analytics.SNIPPET_VERSION="2.0.8",
+window.analytics.load("hdrwb3gk2g");
+window.analytics.page();
+
+
+$(document).on('ready page:change', function() {
+  console.log('page loaded');
+  window.analytics.page();
+  window.analytics.trackForm($('#new_visitor'), 'Signed Up');
+  window.analytics.trackForm($('#new_contact'), 'Contact Request');
 })
