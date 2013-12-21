b8d30475ef46bee788cf1a62a8193f5773e7de11
Michal Kloda <py.emkey@gmail.com>
2013-12-21 19:06:54 +0100

    fix asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ LearnRails::Application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   #email enabled in production
-  config,action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"]}
+  config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"]}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
